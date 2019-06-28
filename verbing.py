a1b55b27ec1bf8902fdb0a3a3a735d0566280029
VaishnaviNG <33370563+VaishnaviNG@users.noreply.github.com>
2019-06-28 07:02:48 +0100

    Using ternary operator

--- verbing.py
+++ verbing.py
@@ -6,19 +6,26 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 
+##def verbing(s):
+##    str_len = len(s)
+##    verb=''   
+##    if str_len<3:
+##        verb=s
+##    else:
+##       if s[str_len-3:str_len]=="ing":
+##            verb = s+'ly'
+##       else:
+##            verb=s+'ing'
+##    return verb
+
+
 def verbing(s):
-    str_len = len(s)
-    verb=''   
-    if str_len<3:
-        verb=s
-    else:
-       if s[str_len-3:str_len]=="ing":
-            verb = s+'ly'
-       else:
-            verb=s+'ing'
-    return verb
+     verb=''
+     verb=(s if len(s)<3 else
+                    (s+'ly' if s[len(s)-3:len(s)]=="ing"
+                     else s+'ing'))
 
-
+     return verb
 
 
 print(verbing("Hi"))
